225d081645029ae6850269b248a0eb3b4cefc677
BixiSG <51515692+BixiSG@users.noreply.github.com>
2019-06-09 02:44:18 +0300

    Update react.js

--- commands/react.js
+++ commands/react.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
-    if(message.author.id != "392082408050130945") return;
+    if(message.author.id != "169131719042007041") return;
     message.react(`🤔`);
 
 }
