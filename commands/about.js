484a6dd8b3ee4016467b9ae21d948c84a1dc48a2
BixiSG <51515692+BixiSG@users.noreply.github.com>
2019-06-10 14:18:09 +0300

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -3,11 +3,12 @@ const Discord = require('discord.js');
 module.exports.run = async (bot, message, args) => {
         let bicon = bot.user.displayAvatarURL;
         let botembed = new Discord.RichEmbed()
-        .setDescription("What the bot can do")
+        .setDescription("CLANWAR Moderation Bot")
         .setColor("#15f153")
         .setThumbnail(bicon)
-        .addField("Bot Name", bot.user.username)
-	.addField("Moderation", "kick, ban, warn, tempmute, addrole, removerole")
+        .addField("Name:", bot.user.username)
+	.addField("Moderation:", "kick, ban, warn, tempmute, addrole, removerole")
+	.addField("Report:", "report and see reports")
         .addField("Created by", "Dark#3668");
 
         return message.channel.send(botembed);
