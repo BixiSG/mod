10ce329ffe6ec9967dba9b2fa624b51e7205f626
BixiSG <51515692+BixiSG@users.noreply.github.com>
2019-06-12 01:54:11 +0300

    Update nsfw.js

--- commands/nsfw.js
+++ commands/nsfw.js
@@ -1,13 +1,13 @@
 const superagent = require('superagent')
 const Discord = require('discord.js')
-exports.run = async (client, message, url, args, tools) => {
+exports.run = async (client, message, link, args, tools) => {
     
     const { body } = await superagent
     .get('http://www.reddit.com/r/randnsfw');
     const embed = new Discord.RichEmbed()
     .setColor(0x954D23)
     .setTitle("NSFW:")
-    .setImage(body.message)
+    .setImage(body.link)
     message.channel.send({embed})
     
 
