ba6ca174de4e67aac472300d0b599a39c68a2adb
BixiSG <51515692+BixiSG@users.noreply.github.com>
2019-06-11 17:45:36 +0300

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -1,13 +1,13 @@
 const superagent = require('superagent')
 const Discord = require('discord.js')
-exports.run = async (client, message, args, tools) => {
+exports.run = async (client, message, url, args, tools) => {
     
     const { body } = await superagent
     .get('https://meme-api.herokuapp.com/gimme');
     const embed = new Discord.RichEmbed()
     .setColor(0x954D23)
     .setTitle("Meme:")
-    .setImage(body.message)
+    .setImage(body.url)
     message.channel.send({embed})
     
 
