50cc072a96524fd02af809410286e8abaea1abb1
BixiSG <51515692+BixiSG@users.noreply.github.com>
2019-06-11 18:07:52 +0300

    Add files via upload

---
+++ commands/link.js
@@ -0,0 +1,51 @@
+const Discord = require('discord.js');
+
+
+exports.run = (client, message, args) => {
+  message.channel.send({embed: {
+    color: 3447003,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL
+    },
+    title: "CLANWAR Bot Links:",
+    url: "https://discordapp.com/api/oauth2/authorize?client_id=582177610000367626&permissions=8&scope=bot",
+    description: "Invite link for bot.",
+    fields: [      
+      {
+        name: "Telegram",
+        value: "[@dark_ones](https://t.me/dark_ones)"
+      },
+      
+      {
+        name: "Discord",
+        value: "[Dark#3668](https://www.reddit.com/user/TheWatcher247)"
+      },
+       
+      {
+        name: "Website",
+        value: "[clanwar.cf](http://clanwar.cf)"
+      },
+      
+    ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL,
+      text: "© CLANWAR - PVPCommunity's Project"
+    }
+  }
+});
+        };
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'link',
+  description: 'shows all related bot links',
+  usage: 'link'
+};
