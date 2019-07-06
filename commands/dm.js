13506f448251cb0970b9611c16a093a79dfa597f
BixiSG <51515692+BixiSG@users.noreply.github.com>
2019-07-07 00:52:49 +0300

    Update dm.js

--- commands/dm.js
+++ commands/dm.js
@@ -8,7 +8,7 @@ module.exports.run = async (bot, message, args) => {
    let dMessage = args.join(" ").slice(22);
    if(dMessage.length < 1) return message.reply('You must supply a message!')
 
-     dUser.send(`**Message from user:** <@!528992402132959244> \n**Message:** ||${dMessage}|| (click to reveal) \n\nhttp://clanwar.cf/donotreply.png`)
+     dUser.send(`**Message from user:** <@!528992402132959244> \n**Message:** ||${dMessage}|| (click to reveal) \n\n__**Automated message. DO NOT REPLY!**__`)
 
      message.author.send(`${message.author} **You have sent your message to** ${dUser}.`)
 
