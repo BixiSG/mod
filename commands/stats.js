df02c79e95fffeeaab216c8c7a8fffa914277221
BixiSG <51515692+BixiSG@users.noreply.github.com>
2019-10-05 18:01:36 +0200

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -4,14 +4,14 @@ require("moment-duration-format");
 
 exports.run = (client, msg, args) => {
     const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
-    msg.channel.send( **TOTAL STATISTICS:** 
+    msg.channel.send(`= TOTAL STATISTICS =
 • Mem Usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
 • Uptime     :: ${duration}
 • Users      :: ${client.users.size.toLocaleString()}
 • Servers    :: ${client.guilds.size.toLocaleString()}
 • Channels   :: ${client.channels.size.toLocaleString()}
 • Discord.js :: v${Discord.version}
-• Node       :: ${process.version}, {code: "asciidoc"});
+• Node       :: ${process.version}`, {code: "asciidoc"});
 };
 
 exports.conf = {
@@ -25,4 +25,4 @@ exports.help = {
   name: 'stats',
   description: 'Gives some useful bot statistics',
   usage: 'stats'
-};
+}; 
