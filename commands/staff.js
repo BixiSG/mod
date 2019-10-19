34292c5e6297123d1146a1f8562913f2efbd3aca
BixiSG <51515692+BixiSG@users.noreply.github.com>
2019-10-19 14:06:52 +0200

    Update staff.js

--- commands/staff.js
+++ commands/staff.js
@@ -6,16 +6,16 @@ module.exports.run = async (bot, message, args) => {
         .setDescription("__**CLANWAR Staff Commands:**__")
         .setColor("#15f153")
         .setThumbnail(bicon)
-.addField("p", "punishment command")
-.addField("logs", "list of the cheat logs")
-.addField("report", "report and see reports")
-.addField("dupeip", "alt accounts checker")
-.addField("history", "punishment history")
-.addField("find", "what server a player is on")
-.addField("vanish", "vanish")
-.addField("tele", "teleportation")
-.addField("mask", "rank masking")
-.addField("silentjoin", "toggle joining message");
+.addField("/p", "punishment command")
+.addField("/logs", "list of the cheat logs")
+.addField("/report", "report and see reports")
+.addField("/dupeip", "alt accounts checker")
+.addField("/history", "punishment history")
+.addField("/find", "what server a player is on")
+.addField("/vanish", "vanish")
+.addField("/tele", "teleportation")
+.addField("/mask", "rank masking")
+.addField("/silentjoin", "toggle joining message");
         return message.channel.send(botembed);
 }
 
