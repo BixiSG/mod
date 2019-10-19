5b6667a5183b963ef1b82704fa14032221a3b86d
BixiSG <51515692+BixiSG@users.noreply.github.com>
2019-10-19 14:05:03 +0200

    Update staff.js

--- commands/staff.js
+++ commands/staff.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 module.exports.run = async (bot, message, args) => {
         let bicon = bot.user.displayAvatarURL;
         let botembed = new Discord.RichEmbed()
-        .setDescription("CLANWAR Staff Commands:")
+        .setDescription("__**CLANWAR Staff Commands:**__")
         .setColor("#15f153")
         .setThumbnail(bicon)
 .addField("p", "punishment command")
