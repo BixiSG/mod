c892d19865c043cbf2a009154d7d1fa90cf947a3
BixiSG <51515692+BixiSG@users.noreply.github.com>
2019-10-19 14:04:11 +0200

    Create staff.js

---
+++ commands/staff.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (bot, message, args) => {
+        let bicon = bot.user.displayAvatarURL;
+        let botembed = new Discord.RichEmbed()
+        .setDescription("CLANWAR Staff Commands:")
+        .setColor("#15f153")
+        .setThumbnail(bicon)
+.addField("p", "punishment command")
+.addField("logs", "list of the cheat logs")
+.addField("report", "report and see reports")
+.addField("dupeip", "alt accounts checker")
+.addField("history", "punishment history")
+.addField("find", "what server a player is on")
+.addField("vanish", "vanish")
+.addField("tele", "teleportation")
+.addField("mask", "rank masking")
+.addField("silentjoin", "toggle joining message");
+        return message.channel.send(botembed);
+}
+
+module.exports.help = {
+        name: "staff" 
+}
