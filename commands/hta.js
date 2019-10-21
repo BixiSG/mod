52db78f7332ee9dad80bda1141c7b65af0dbb33d
BixiSG <51515692+BixiSG@users.noreply.github.com>
2019-10-21 19:39:02 +0200

    Update hta.js

--- commands/hta.js
+++ commands/hta.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot, message, args) => {
    if(!message.member.hasPermission("BAN_MEMBERS")) return message.reply("You can't use that command!")
    let dMessage = args.join(" ").slice(22);
 
-     dUser.send(`__**HOW TO APPEAL YOUR BAN/MUTE:**__ \n Go to #support channel and press :no_entry: emoji. \n\n __**КАК ПОДАТЬ АППИЛЯЦИЮ:**__ \n Перейдите в канал #support и нажмите на :no_entry: эмоцию. \n\n__**Automated message. DO NOT REPLY!**__`)
+     dUser.send(`:pencil: __**HOW TO APPEAL YOUR BAN/MUTE:**__ \n Go to #:gear:-support channel and press :no_entry_sign: emoji.\n\n:flag_ru: __**КАК ПОДАТЬ АПЕЛЛЯЦИЮ:**__\nПерейдите в канал #:gear:-support и нажмите на :no_entry_sign: эмоцию.\n:flag_tr: __**BAN VEYA MUTEYE NASIL İTİRAZ EDİLİR:**__\n#:gears:-support kanalına git ve :no_entry_sign: emojisine tıkla.`)
 
      message.author.send(`${message.author} **You have sent HTA Guide to** ${dUser}.`)
 
