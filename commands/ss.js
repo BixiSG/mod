367c6abdb43440ac939dcc1758dde894d1517ed8
BixiSG <51515692+BixiSG@users.noreply.github.com>
2019-10-27 13:43:22 +0100

    Create ss.js

---
+++ commands/ss.js
@@ -0,0 +1,18 @@
+const superagent = require('superagent')
+const Discord = require('discord.js')
+exports.run = async (client, message, args, tools) => {
+    
+    const { body } = await superagent
+    .get('https://mcapi.us/server/status?ip=clanwar.cf');
+    const embed = new Discord.RichEmbed()
+    .setColor(0x0000)
+    .setTitle("Minecraft Server Stats")
+    .addField("Online players:", players.now)
+    message.channel.send({embed})
+    
+
+}
+
+module.exports.help = {
+    name: "ss"
+}
