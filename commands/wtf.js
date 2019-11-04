7d8e62c38854ea73475335d0de04f18eca9991b0
BixiSG <51515692+BixiSG@users.noreply.github.com>
2019-11-04 18:18:06 +0100

    Update wtf.js

--- commands/wtf.js
+++ commands/wtf.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js')
+const yowtf = "./images/w6dfda0adjl21.jpg";
 exports.run = async (client, message, args, tools) => {
-    const yowtf = "./images/w6dfda0adjl21.jpg";
     const embed = new Discord.RichEmbed()
     .setColor(0x0000)
     .setImage(yowtf)
