be5605b7b790451b357bab260115f9df4e469e36
BixiSG <51515692+BixiSG@users.noreply.github.com>
2019-11-04 18:31:40 +0100

    Update ss.js

--- commands/ss.js
+++ commands/ss.js
@@ -3,10 +3,11 @@ const Discord = require('discord.js')
 exports.run = async (client, message, args, tools) => {
     
     const { body } = await superagent
+    let bicon = bot.user.displayAvatarURL;
     .get('https://mcapi.us/server/status?ip=clanwar.cf');
     const embed = new Discord.RichEmbed()
     .setColor(0x0000)
-    .setThumbnail(bot.user.displayAvatarURL)
+    .setThumbnail(bicon)
     .setTitle("Minecraft Server Stats:")
     .addField("Online players:", players.now)
     message.channel.send(embed)
