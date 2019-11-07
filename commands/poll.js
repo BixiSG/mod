6e86078f03bc0722b6cd18a379e1d39e782dd718
BixiSG <51515692+BixiSG@users.noreply.github.com>
2019-11-07 22:33:46 +0200

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -1,22 +1,19 @@
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
-      if (!message.guild.member(client.user).hasPermission('ADD_REACTIONS')) return message.reply(':x: Missing ADD_REACTIONS perms.')
+      if (!message.guild.member(client.user).hasPermission('ADD_REACTIONS')) return message.reply(':x: Missing `ADD_REACTIONS` perms.')
       const sayMessage = args.join(" ");
-     if (sayMessage.length < 1) return message.channel.send(":x: Usage `-poll text`")
+     if (sayMessage.length < 1) return message.channel.send(":x: Usage: `-poll text`")
      if (message.member.hasPermission("KICK_MEMBERS")) {
        const embed = new Discord.RichEmbed()
-       .setColor(#800080)
-       .setTitle(" Poll: ")
-       .setDescription(`**${sayMessage}**`)
+       .setColor(800080)
+       .setTitle(`**${sayMessage}**`)
         message.channel.send(embed).then(m => {
             m.react('✅');
             m.react('❌');
-            
            })
- 
       }
 }
-  
+
   exports.help = {
     name: 'poll',
     description: 'runs react poll',
