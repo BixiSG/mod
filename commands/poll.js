957600b39a5ca90a09f073b484e878fca58e7f19
BixiSG <51515692+BixiSG@users.noreply.github.com>
2019-11-07 22:17:56 +0200

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -1,35 +1,20 @@
-const Discord = require('discord.js');
-
-exports.run = async (client, message, args, tools) => {
-  
-  if (!message.member.hasPermission('MANAGE_GUILD') && message.author.id !== '392082408050130945') return message.channels.send('Sorry, you don\'t have permission to create poll!').then(msg => msg.delete({timeout: 10000}));
-  if (!args.join(' ')) return message.channel.send('Usage: poll <title>').then(msg => msg.delete({timeout: 10000}));
-  
-  const embed = new Discord.MessageEmbed()
-    .setTitle(args.join(' '))
-    .setFooter('React to vote on Poll!')
-    .setColor('#7289DA')
-    const pollTitle = await message.channel.send({ embed });
-      await pollTitle.react(`👍`);
-      await pollTitle.react(`👎`);
-  
-    const filter = (reaction) => reaction.emoji.name === '👍';
-    const collector = pollTitle.createReactionCollector(filter, { time: 15000 });
-      collector.on('collect', r => console.log(`Collected ${r.emoji.name}`));
-      collector.on('end', collected => console.log(`Collected ${collected.size} items`));
-  
-    const filter1 = (reaction) => reaction.emoji.name === '👎';
-    const collector1 = pollTitle.createReactionCollector(filter1, { time: 15000 });
-      collector1.on('collect', r => console.log(`Collected ${r.emoji.name}`));
-      collector1.on('end', collected => console.log(`Collected ${collected.size} items`));
-};
-
-exports.conf = {
-    enabled: true,
-    guildOnly: false,
-    aliases: [],
-    permLevel: 0
-  };
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+exports.run = (client, message, args) => {
+      if (!message.guild.member(client.user).hasPermission('ADD_REACTIONS')) return message.reply(':x: Missing ADD_REACTIONS perms')
+      const sayMessage = args.join(" ");
+     if (sayMessage.length < 1) return message.channel.send(":x: Nothing to add to the poll.")
+     if (message.member.hasPermission("KICK_MEMBERS")) {
+       const embed = new Discord.RichEmbed()
+       .setColor(0x00A2E8)
+       .setTitle(" Poll ")
+       .setDescription(`"**${sayMessage}**"`)
+        message.channel.send(embed).then(m => {
+            m.react('✅');
+            m.react('❌');
+           })
+      }
+}
   
   exports.help = {
     name: 'poll',
