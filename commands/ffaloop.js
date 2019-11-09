aebe443f2d1533c61e70802e9a08df852954a394
BixiSG <51515692+BixiSG@users.noreply.github.com>
2019-11-09 19:53:43 +0200

    Create ffaloop.js

---
+++ commands/ffaloop.js
@@ -0,0 +1,29 @@
+var Discord = require('discord.js');
+const superagent = require("superagent");
+
+bot.on('message', function(message) {
+    if (message.content === "$loop") { 
+        var interval = setInterval (function () {
+            module.exports.run = async (bot,message,args) => {
+
+  let{body} = await superagent
+  .get(`https://mcapi.us/server/status?ip=clanwar.cf&port=25567`);
+
+  let embed = new Discord.RichEmbed()
+  .setColor("#6a0dad")
+  .setTitle("FFA Server Information:")
+  .setThumbnail('https://i.ibb.co/M9KTXGM/FFA.png')
+  .addField("Online Status:", `**${body.online}**`)
+  .addField("Players Online:", `**${body.players.now}**/${body.players.max}`)
+  .addField("Core Version:", `${body.server.name}`)
+  .setFooter(`clanwar.cf`, 'https://i.ibb.co/RP8JT1h/cw-server-logo.png')
+  message.channel.send(embed)
+}
+            .catch(console.error); 
+        }, 1 * 1000); 
+    }
+});
+
+  module.exports.help = {
+  name: "ffaloop"
+}
