da53019676f2b4fff5a54bcf2980568b465f4854
BixiSG <51515692+BixiSG@users.noreply.github.com>
2019-11-09 20:36:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ module.exports.run = async (bot,message,args) => {
   let{body} = await superagent
   .get(`https://mcapi.us/server/status?ip=clanwar.cf&port=25567`);
 
-   let ffaloop = new Discord.RichEmbed()
+  let ffaloop = new Discord.RichEmbed()
   .setColor("#6a0dad")
   .setTitle("FFA Server Information:")
   .setThumbnail('https://i.ibb.co/M9KTXGM/FFA.png')
@@ -61,16 +61,17 @@ module.exports.run = async (bot,message,args) => {
   .addField("Players Online:", `**${body.players.now}**/${body.players.max}`)
   .addField("Core Version:", `${body.server.name}`)
   .setFooter(`clanwar.cf`, 'https://i.ibb.co/RP8JT1h/cw-server-logo.png')
-   }
+}
 
 bot.on('message', function(message) {
-    if (message.content === "$$$ffa$$$") { 
+    if (message.content === "$ffa") { 
         var interval = setInterval (function () {
             message.channel.send(ffaloop)
-            .catch(console.error); 
-        }, 1 * 60000); 
+            .catch(console.error);
+        }, 1 * 10000); 
     }
 });
+
 
 //------ start first part of invite module ------------ //
 
