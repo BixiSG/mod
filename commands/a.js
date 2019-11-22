623430e4831966a2e44d4503b5f73a516ecaa4c7
BixiSG <51515692+BixiSG@users.noreply.github.com>
2019-11-22 02:53:05 +0200

    Update a.js

--- commands/a.js
+++ commands/a.js
@@ -1,4 +1,12 @@
+const Discord = require("discord.js");
 const args = message.content.slice(prefix.length).split(' ');
 const command = args.shift().toLowerCase();
 
-else if (command === 'a') { 	if (!args.length) { 		return message.channel.send(`You didn't provide any arguments, ${message.author}!`); 	} 	message.channel.send(`Command name: ${command}\nArguments: ${args}`); }
+module.exports.run = async (bot, message, args) => {
+
+if (command === 'a') { 	if (!args.length) { 		return message.channel.send(`You didn't provide any arguments, ${message.author}!`); 	} 	message.channel.send(`Command name: ${command}\nArguments: ${args}`); }
+}
+
+module.exports.help = {
+    name: 'a'
+}
