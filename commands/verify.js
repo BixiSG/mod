74a68a5d19a694823a6a4ef7c20a7c7a98177422
BixiSG <51515692+BixiSG@users.noreply.github.com>
2019-11-22 19:45:54 +0200

    Update verify.js

--- commands/verify.js
+++ commands/verify.js
@@ -3,19 +3,19 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     
     let role = message.guild.roles.find(role => role.name === '✔');
-    if (message.channel.name !== 'verification') return message.reply('You must go to the channel #verification');
+    if (message.channel.name !== 'verification') return message.reply(':x: You must go to the channel #verification');
     message.member.addRole(role);
     if (message.member.roles.has(role.id)) {
         let verifyEmbed = new Discord.RichEmbed()
             .setAuthor(message.member.displayName, message.author.displayAvatarURL)
             .setColor('#eeee00')
-            .setDescription('Your account has already been verified!')
+            .setDescription(':x: Your account has already been verified!')
         return message.channel.send((verifyEmbed));
     } else {
         let verifyEmbed = new Discord.RichEmbed()
             .setAuthor(message.member.displayName, message.author.displayAvatarURL)
             .setColor('#008000')
-            .setDescription('Your account has been successfully verified.')
+            .setDescription(':white_check_mark: Your account has been successfully verified.')
         return message.channel.send((verifyEmbed));
     }
 }
