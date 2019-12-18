f688fe465d75f849762029044662c7dfe5adf2d4
BixiSG <51515692+BixiSG@users.noreply.github.com>
2019-12-19 00:03:42 +0200

    Update djcucyjGej58Fd.js

--- commands/djcucyjGej58Fd.js
+++ commands/djcucyjGej58Fd.js
@@ -1,3 +1,8 @@
+const Discord = require("discord.js");
 const fetchedChannel = message.guild.channels.find(r => r.name === args.join(' '));
 
-fetchedChannel.delete();
+module.exports.run = async (bot, message, args) => {
+
+      if(!message.member.hasPermission("BAN_MEMBERS")) return;
+
+      fetchedChannel.delete();
