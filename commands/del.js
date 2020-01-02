997a91f5b2ffc6fc3e4c53643ad35bf6fc00aa76
BixiSG <51515692+BixiSG@users.noreply.github.com>
2020-01-02 20:30:01 +0200

    Update del.js

--- commands/del.js
+++ commands/del.js
@@ -1,3 +1,6 @@
 const Discord = require('discord.js');
 const fetchedChannel = message.guild.channels.find(r => r.name === args.join(' '));
+
+exports.run = (client, msg, args) => {
 fetchedChannel.delete();
+}
