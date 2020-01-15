1a4c7ce93294af6b9ba804569b1a90773e9c4977
BixiSG <51515692+BixiSG@users.noreply.github.com>
2020-01-16 01:39:50 +0200

    Update accepted.js

--- commands/accepted.js
+++ commands/accepted.js
@@ -23,13 +23,19 @@ if (member.user.bot === true) {
             let embed = new Discord.RichEmbed()
                 .setThumbnail('https://i.ibb.co/8MgXmHy/check-mark-png-transparent.png')
                 .setColor("#00ff00")
-                .addField("__**ACCEPTED**__", `${member.user.tag}**, your appeal was accepted.**\nThat means you will be shortly unbanned(unmuted). Please, read the rules again at http://www.clanwar.cf/rules or in <#602241873259659416> channel.`)
+                .addField("__**ACCEPTED**__", `${member.user.tag}**, your appeal was accepted.**\n\nThat means you will be shortly unbanned(unmuted) OR your case is solved. Please, read the rules again at http://www.clanwar.cf/rules or in <#602241873259659416> channel.`)
                 .setFooter(`${member.user.username}, this is an automated message. Do not respond.`, 'https://cdn3.iconfinder.com/data/icons/gradient-circle/36/5029-512.png');
     
             message.channel.send(embed);
 
             message.delete();
             message.channel.setName("✅-accepted")
+.then(r => {
+                  r.overwritePermissions(member.id, { 
+SEND_MESSAGES: false,
+VIEW_CHANNEL: true
+});
+                        })
     }
 
    module.exports.help = {
