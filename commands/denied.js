324d802388a236043122a955cec48faffd624eaa
BixiSG <51515692+BixiSG@users.noreply.github.com>
2020-01-15 20:50:02 +0200

    Update denied.js

--- commands/denied.js
+++ commands/denied.js
@@ -11,7 +11,7 @@ let target = message.mentions.users.first() || message.author
             let embed = new Discord.RichEmbed()
                 .setThumbnail((target.displayAvatarURL))
                 .setColor("#ff0000")
-                .addField("**DENIED**", `${member.user.tag}, your appeal was denied. That means you need to wait out your ban(mute) or purchase an unban(unmute) at http://store.clanwar.cf in the *Pardons* category.`)
+                .addField("**DENIED**", "${member.user.tag}, your appeal was denied. That means you need to wait out your ban(mute) or purchase an unban(unmute) at http://store.clanwar.cf in the *Pardons* category.")
                 .setFooter(`${member.user.username}, this is an automated message. Do not respond.`, 'https://cdn3.iconfinder.com/data/icons/gradient-circle/36/5029-512.png')
     
             message.channel.send(embed);
