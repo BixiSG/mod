26ab70d108b37f1ec4168164f4013a337ea80b56
BixiSG <51515692+BixiSG@users.noreply.github.com>
2020-01-15 21:00:02 +0200

    Update denied.js

--- commands/denied.js
+++ commands/denied.js
@@ -3,18 +3,34 @@ const Discord = require("discord.js");
 module.exports.run =async (bot, message, args) => {
     let inline = true
     let resence = true
+    const status = {
+        online: ":green_heart: Online",
+        idle: ":yellow_heart: Idle",
+        dnd: ":no_entry: Do Not Disturb",
+        offline: ":grey_question: Offline/Invisible"
+      }
         
 const member = message.mentions.members.first() || message.guild.members.get(args[0]) || message.member;
 let target = message.mentions.users.first() || message.author
+
+if (member.user.bot === true) {
+    bot = ":robot: Yes";
+  } else {
+    bot = ":x::robot: No";
+  }
 
 
             let embed = new Discord.RichEmbed()
                 .setThumbnail((target.displayAvatarURL))
                 .setColor("#ff0000")
                 .addField("**DENIED**", `${member.user.tag}, your appeal was denied. That means you need to wait out your ban(mute) or purchase an unban(unmute) at http://store.clanwar.cf in the *Pardons* category.`)
-                .setFooter(`${member.user.username}, this is an automated message. Do not respond.`, 'https://cdn3.iconfinder.com/data/icons/gradient-circle/36/5029-512.png')
+                .setFooter(`${member.user.username}, this is an automated message. Do not respond.`, 'https://cdn3.iconfinder.com/data/icons/gradient-circle/36/5029-512.png');
     
             message.channel.send(embed);
 
             message.delete();
+    }
+
+   module.exports.help = {
+        name: "denied"
     }
