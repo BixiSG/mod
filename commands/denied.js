a721455641870ea7978085e6c4b5de25d68cf1b0
BixiSG <51515692+BixiSG@users.noreply.github.com>
2020-01-15 23:18:47 +0200

    Update denied.js

--- commands/denied.js
+++ commands/denied.js
@@ -30,6 +30,7 @@ if (member.user.bot === true) {
 
             message.delete();
             message.channel.setName("❌-denied")
+            channel.permissionOverwrites.get(message.member).delete();
             channel.overwritePermissions(channel.guild.member, { SEND_MESSAGES: false });
     }
 
