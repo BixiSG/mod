0a24619ee812e26a18a6a2203030ea6be92f51f5
BixiSG <51515692+BixiSG@users.noreply.github.com>
2020-01-20 20:30:39 +0200

    Update lock.js

--- commands/lock.js
+++ commands/lock.js
@@ -1,12 +1,11 @@
 const Discord = require("discord.js");
 
-module.exports.run =async (bot, message, args) => {
+module.exports.run = async (bot, message, args) => {
 
-channel.overwritePermissions(channel.guild.defaultRole, { SEND_MESSAGES: false })
-.then(() => {
-            message.channel.send("**:white_check_mark: Channel is now locked.**")
-            message.delete();
-            }
+    channel.overwritePermissions(channel.guild.defaultRole, { SEND_MESSAGES: false }).then(() => { 
+    message.channel.send("**:white_check_mark: Channel is now locked.**");
+            message.delete()
+    }
 };
 
 exports.help = {
