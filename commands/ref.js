930adf9749ee08c601fb68b3dbfa7ce4151d9eb1
BixiSG <51515692+BixiSG@users.noreply.github.com>
2020-04-30 16:50:40 +0300

    Create ref.js

---
+++ commands/ref.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const snekfetch = require('snekfetch');
+exports.run = async (client, message, args) => {
+	try {
+        const { body } = await snekfetch
+            .get('https://www.reddit.com/r/ArchitecturePorn.json?sort=top&t=week')
+            .query({ limit: 800 });
+        const allowed = message.channel.nsfw ? body.data.children : body.data.children.filter(post => !post.data.over_18);
+        if (!allowed.length) return message.channel.send(':x: Out of fresh pictures. Try again later.');
+        const randomnumber = Math.floor(Math.random() * allowed.length)
+        const embed = new Discord.RichEmbed()
+        .setColor(#A2E800)
+        .setTitle(allowed[randomnumber].data.title)
+        .setImage(allowed[randomnumber].data.url)
+        .addField("Info:", "Up votes: " + allowed[randomnumber].data.ups + " / Comments: " + allowed[randomnumber].data.num_comments)
+        message.channel.send(embed)
+    } catch (err) {
+        return console.log(err);
+    }
+}
+
+module.exports.help = {
+    name: "ref"
+}
