187f0ba12af014f4fa1e6aa06499fa11a83f74c7
BixiSG <51515692+BixiSG@users.noreply.github.com>
2020-04-30 17:18:43 +0300

    Create land.js

---
+++ commands/land.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+const snekfetch = require('snekfetch');
+exports.run = async (client, message, args) => {
+	let msg = await message.channel.send(":eyes: Looking for pictures...");
+	try {
+        const { body } = await snekfetch
+            .get('https://www.reddit.com/r/earthporn.json?sort=top&t=week')
+            .query({ limit: 800 });
+        const allowed = message.channel.nsfw ? body.data.children : body.data.children.filter(post => !post.data.over_18);
+        if (!allowed.length) return message.channel.send('No fresh memes. Try again later.');
+        const randomnumber = Math.floor(Math.random() * allowed.length)
+        const embed = new Discord.RichEmbed()
+        .setColor(0x9900FF)
+        .setTitle(allowed[randomnumber].data.title)
+        .setDescription("Posted by: " + allowed[randomnumber].data.author)
+        .setImage(allowed[randomnumber].data.url)
+        .setFooter("From r/EarthPorn")
+        message.channel.send(embed)
+	message.delete()
+	msg.delete()
+    } catch (err) {
+        return console.log(err);
+    }
+}
+
+module.exports.help = {
+    name: "land"
+}
