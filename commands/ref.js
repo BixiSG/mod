0eb095d51862b9040333902992fa13df17a922f7
BixiSG <51515692+BixiSG@users.noreply.github.com>
2020-04-30 16:57:46 +0300

    Update ref.js

--- commands/ref.js
+++ commands/ref.js
@@ -3,17 +3,18 @@ const snekfetch = require('snekfetch');
 exports.run = async (client, message, args) => {
 	try {
         const { body } = await snekfetch
-            .get('https://www.reddit.com/r/ArchitecturePorn.json?sort=top&t=week')
+            .get('https://www.reddit.com/r/architectureporn.json?sort=top&t=week')
             .query({ limit: 800 });
         const allowed = message.channel.nsfw ? body.data.children : body.data.children.filter(post => !post.data.over_18);
-        if (!allowed.length) return message.channel.send(':x: Out of fresh pictures. Try again later.');
+        if (!allowed.length) return message.channel.send('No fresh memes. Try again later.');
         const randomnumber = Math.floor(Math.random() * allowed.length)
         const embed = new Discord.RichEmbed()
-        .setColor(#A2E800)
+        .setColor(0x00A2E8)
         .setTitle(allowed[randomnumber].data.title)
-	.setDescription("By: " + allowed[randomnumber].data.author)
+        .setDescription("By: " + allowed[randomnumber].data.author)
         .setImage(allowed[randomnumber].data.url)
         .addField("Info:", "Up votes: " + allowed[randomnumber].data.ups + " / Comments: " + allowed[randomnumber].data.num_comments)
+        .setFooter("From r/ArchitecturePorn")
         message.channel.send(embed)
     } catch (err) {
         return console.log(err);
