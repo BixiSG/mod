48ad3ad2e40544b30721470e29403c502663a3ee
BixiSG <51515692+BixiSG@users.noreply.github.com>
2020-04-30 21:38:55 +0300

    Update ffa.js

--- commands/ffa.js
+++ commands/ffa.js
@@ -2,9 +2,10 @@ const Discord = require("discord.js");
 const superagent = require("superagent");
 
 module.exports.run = async (bot,message,args) => {
-
+if(!message.member.hasPermission("BAN_MEMBERS")) return;
+  
   let{body} = await superagent
-  .get(`https://mcapi.us/server/status?ip=clanwar.cf&port=25567`);
+  .get(`https://mcapi.us/server/status?ip=clanwar.cc&port=25567`);
 
   let embed = new Discord.RichEmbed()
   .setColor("#6a0dad")
@@ -13,7 +14,7 @@ module.exports.run = async (bot,message,args) => {
   .addField("Online Status:", `**${body.online}**`)
   .addField("Players Online:", `**${body.players.now}**/${body.players.max}`)
   .addField("Core Version:", `${body.server.name}`)
-  .setFooter(`clanwar.cf`, 'https://i.ibb.co/RP8JT1h/cw-server-logo.png')
+  .setFooter(`clanwar.cc`, 'https://i.ibb.co/RP8JT1h/cw-server-logo.png')
   message.channel.send(embed);
 }
         
