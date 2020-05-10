fc27727372819c3dd4ca0a263f0da9269c386c7c
BixiSG <51515692+BixiSG@users.noreply.github.com>
2020-05-10 12:11:47 +0300

    Update bru.js

--- commands/bru.js
+++ commands/bru.js
@@ -1,7 +1,9 @@
 const Discord = require("discord.js");
+const attachment = new MessageAttachment('https://i.imgur.com/w3duR07.png');
+    
 
 exports.run = (client, msg, args) => {
-message.channel.send("damn boy", {files: ["https://i.imgur.com/TubNuP5.png"]});
+message.channel.send(attachment);
 };
 
 exports.help = {
