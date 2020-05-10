73f06335f3f8a8b0d0f416b3db6efc693d30f1af
BixiSG <51515692+BixiSG@users.noreply.github.com>
2020-05-10 12:17:17 +0300

    Update bru.js

--- commands/bru.js
+++ commands/bru.js
@@ -1,4 +1,4 @@
-const Discord = require("discord.js");
+const { Discord, Client, MessageAttachment } = require('discord.js');
 const attachment = new MessageAttachment('https://media.tenor.com/images/d9de4038d32d39d6064b1b46e05c29b4/tenor.gif');
     
 
