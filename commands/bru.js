f1558b9f0d1ca0543e3e1a1947400e96d2da8569
BixiSG <51515692+BixiSG@users.noreply.github.com>
2020-05-10 12:33:37 +0300

    Update bru.js

--- commands/bru.js
+++ commands/bru.js
@@ -1,6 +1,10 @@
 const Discord = require('discord.js');
 const file = new Discord.MessageAttachment('https://media.tenor.com/images/d9de4038d32d39d6064b1b46e05c29b4/tenor.gif');
 
+module.exports.run =async (bot, message, args) => {
+
+if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(":x: Permission denied.");
+	
 const bruEmbed = {
 	title: 'bru',
 	image: {
@@ -9,7 +13,9 @@ const bruEmbed = {
 };
 
 channel.send({ files: [file], embed: bruEmbed });
-
+message.delete();
+};
+	
 exports.help = {
   name: 'bru',
-}; 
+};
