168b1835c2131e47039a2ca27e0c740625e22e65
BixiSG <51515692+BixiSG@users.noreply.github.com>
2020-05-10 12:07:22 +0300

    Create bru.js

---
+++ commands/bru.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js");
+
+exports.run = (client, msg, args) => {
+message.channel.send("damn boy", {files: ["https://i.imgur.com/TubNuP5.png"]});
+};
+
+exports.help = {
+  name: 'bru',
+}; 
