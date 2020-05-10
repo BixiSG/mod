b60147b7914eaee05402f205a4501e12b01e065c
BixiSG <51515692+BixiSG@users.noreply.github.com>
2020-05-10 12:27:24 +0300

    Update bru.js

--- commands/bru.js
+++ commands/bru.js
@@ -1,10 +1,14 @@
 const { Discord, Client, MessageAttachment } = require('discord.js');
-const attachment = new MessageAttachment('https://media.tenor.com/images/d9de4038d32d39d6064b1b46e05c29b4/tenor.gif');
-    
+const file = new Discord.MessageAttachment('https://media.tenor.com/images/d9de4038d32d39d6064b1b46e05c29b4/tenor.gif');
 
-exports.run = (client, msg, args) => {
-channel.send(`${message.author},`, attachment);
+const bruEmbed = {
+	title: 'bru',
+	image: {
+		url: 'https://media.tenor.com/images/d9de4038d32d39d6064b1b46e05c29b4/tenor.gif',
+	},
 };
+
+channel.send({ files: [file], embed: bruEmbed });
 
 exports.help = {
   name: 'bru',
