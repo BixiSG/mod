bc4a0825351a480c0aafb35409e791669737160d
BixiSG <51515692+BixiSG@users.noreply.github.com>
2020-07-13 00:21:41 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
 let helpembed = new Discord.RichEmbed()
 .setDescription("**HELP MENU**")
 .setColor("#6a0dad")
-.addField("Discord & Server support:", "\<618903822319747072>")
+.addField("Discord & Server support:", "<#618903822319747072>")
 .addField("How to report a user:", "`-report [@user] [reason]`")
 
 message.channel.send(helpembed);
